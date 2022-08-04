f4876e616d6eeee1ee8637f3af5e929f432126f4
Awang <awangmedidat98@gmail.com>
2022-08-04 13:59:48 +0700

    intialitate

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Let's Build Netflix
+      {/* Header */}
+      <main>
+        {/* Banner */}
+        <section>{/* Row */}</section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
